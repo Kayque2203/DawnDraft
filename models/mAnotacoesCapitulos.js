b6aadfd3255b9c55a1fd24751c653b941ed33354
Enzo-1022 <enzofernandes560@gmail.com>
2024-09-29 23:03:28 -0300

    Comecei a fazer a funcionalidade das anotações.
    Criei as classes modelo para as coleções de anotações e dos personagens que irão conter a anotação.

---
+++ models/mAnotacoesCapitulos.js
@@ -0,0 +1,55 @@
+const { ObjectId } = require('mongodb');
+const conexao = require('../conexaoBD/conexaoBD');
+
+const Conexao = new conexao();
+
+class Anotacoes {
+    focoCapitulo;
+    humorTom;
+    capitulo;
+
+    constructor(focoCap, humroTomCap, capituloAnotacao){
+        this.focoCapitulo = focoCap;
+        this.humorTom = humroTomCap;
+        this.capitulo = capituloAnotacao;
+    }
+
+    // Métodos da classe
+    async adicionarAnotacao(){
+        let novaAnotacao = await Conexao.getCollections('Anotacoes').insertOne({
+            "FocoCapitulo" : this.focoCapitulo,
+            "HumorTom" : this.humorTom,
+            "Capitulo" : new ObjectId(this.capitulo)
+        });
+
+        return novaAnotacao.insertedId.toString();
+    }
+
+    async atualizarAnotacao(idAnotacao){
+        await Conexao.getCollections('Anotacoes').updateOne(
+            {_id : new ObjectId(idAnotacao)},
+            {
+                $set: {
+                    "FocoCapitulo" : this.focoCapitulo,
+                    "HumorTom" : this.humorTom
+                },
+                $currentDate: { lastModified: true }
+            }
+        );
+    }
+
+    // Métodos staticos
+    static async buscaAnotacoes(idCapitulo){
+        let todasAnotacoes = await Conexao.getCollections('Anotacoes').find({"Capitulo" : new ObjectId(idCapitulo)}).toArray();
+
+        return todasAnotacoes;
+    }
+
+    static async deletarAnotacao(idAnotacao){
+        let anotacaoDeletada = await Conexao.getCollections('Anotacoes').deleteOne({_id: new ObjectId(idAnotacao)});
+
+        return anotacaoDeletada;
+    }
+}
+
+module.exports = Anotacoes;
