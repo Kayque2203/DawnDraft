ea09d12e314f3a06cf5201e6aa84c4e39976274b
Enzo-1022 <enzofernandes560@gmail.com>
2024-12-01 00:27:05 -0300

    Lapidando o FrontEnd

--- public/javascripts/excluir.js
+++ public/javascripts/excluir.js
@@ -3,6 +3,8 @@ const btnCancelar = document.getElementById('btnCancelar');
 const spanDeletarHis = document.getElementsByClassName('spanDeletarHis')[0];
 const sectionConteudo = document.getElementById('sectionConteudo');
 
+const sectionConteudo2 = document.getElementById('sectionConteudo2');
+
 const body = document.getElementsByTagName('body')[0]
 
 spanDeletarHis.addEventListener('click', () => {
@@ -10,10 +12,12 @@ spanDeletarHis.addEventListener('click', () => {
     window.scroll(0,0);
     sectionConteudo.style.opacity = "0";
     body.style.backgroundColor = "grey";
+    sectionConteudo2.style.opacity = "0";
 })
 
 btnCancelar.addEventListener('click', () => {
     divExcluirPersonagem.style.display = "none";
     sectionConteudo.style.opacity = "1";
     body.style.backgroundColor = "#fcfbee";
+    sectionConteudo2.style.opacity = "1 ";
 })
